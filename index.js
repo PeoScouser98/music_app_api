3bb53498ccd633bfc6eb89cc6658682aa5d0f514
quanghiep031 <quanghiep03198@gmail.com>
2022-09-01 01:31:57 +0700

    change db path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import app from "./src/app";
 import "dotenv/config";
 /* ======================== Run Server ============================= */
+
 app.listen(process.env.PORT, () => {
 	console.log(`Connected! Server is listening on: http://localhost:${process.env.PORT}`);
 });
