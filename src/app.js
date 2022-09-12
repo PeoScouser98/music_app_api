a6b76c8837b71c00a0521d03933088a1a7687d2f
quanghiep031 <quanghiep03198@gmail.com>
2022-09-12 10:52:04 +0700

    routers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import "dotenv/config";
-import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
 /* ============= Import Routers ============== */
-import trackRouter from "./routes/track";
-import userRouter from "./routes/user";
-import artistRouter from "./routes/artist";
-import genreRouter from "./routes/genre";
-import playListRouter from "./routes/playlist";
+import trackRouter from "./routes/track.route";
+import userRouter from "./routes/user.route";
+import artistRouter from "./routes/artist.route";
+import genreRouter from "./routes/genre.route";
+import playListRouter from "./routes/playlist.route";
 
 const app = express();
 
@@ -23,10 +22,20 @@ app.use(express.json()); // using JSON data type
 /* ==================== Using Routers ========================*/
 /* ========================================================== */
 app.get("/", (req, res) => {
-	res.send("Server now is running!");
+	try {
+		res.send("<h1>Server now is running!</h1>");
+	} catch (error) {
+		res.status(404).send("Server is stopped!");
+	}
 });
 app.get("/upload-track", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "./views/upload.html"));
+});
+app.get("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.get("/activate-account", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/verify_account.html"));
 });
 
 app.use("/api", trackRouter);
