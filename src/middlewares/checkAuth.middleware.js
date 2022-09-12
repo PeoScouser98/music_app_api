4e1d1f8eb31bd1e4d98cf58051582daf4951f6b6
quanghiep031 <quanghiep03198@gmail.com>
2022-09-12 10:52:30 +0700

    check auth middleware

--- src/middlewares/checkAuth.middleware.js
+++ src/middlewares/checkAuth.middleware.js
@@ -1,16 +1,16 @@
-import User from "../models/user";
+import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-import fs from "fs";
-import path from "path";
+import jwtConfig from "../config/jwt.config";
 
 export const requireSignin = async (req, res, next) => {
 	try {
-		const certification = fs.readFileSync(path.resolve(__dirname, "../config/keys/private.pem"));
-		const userId = jwt.verify(req.body.token, certification, { algorithms: "RS256" });
+		// const userId = jwt.verify(req.body.token, certification, { algorithms: "RS256" });
+		const token = req.body.token || req.query.token;
+		const userId = await jwt.verify(token, jwtConfig.certification);
 		if (!userId)
 			return res.status(400).json({
-				message: "Bạn phải đăng nhập để sử dụng tính năng này!",
+				message: "You haven't login yet!",
 			});
 		req.role = await User.findOne({ _id: userId.id }, { role: 1 });
 		next();
