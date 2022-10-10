c7f1b1bd950737411699738b4cd2e8f1e4d1818d
quanghiep031 <quanghiep03198@gmail.com>
2022-10-10 23:11:37 +0700

    updated on genre

--- src/routes/genre.route.js
+++ src/routes/genre.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { list, read, create, update, del } from "../controllers/genre.controller";
-import { requireSignin } from "../middlewares/checkAuth.middleware";
+import { checkAccessToken } from "../middlewares/checkAuth.middleware";
 const router = express.Router();
 
 router.get("/genre", list);
 router.get("/genre/:id", read);
-router.post("/genre", requireSignin, create);
-router.patch("/genre/:id", requireSignin, update);
-router.delete("/genre/:id", requireSignin, del);
+router.post("/genre", checkAccessToken, create);
+router.patch("/genre/:id", checkAccessToken, update);
+router.delete("/genre/:id", checkAccessToken, del);
 
 export default router;
