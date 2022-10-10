79fd5d0329ff1490051c872cf71a703bb4367578
quanghiep031 <quanghiep03198@gmail.com>
2022-10-10 23:10:55 +0700

    check auth

--- src/middlewares/checkAuth.middleware.js
+++ src/middlewares/checkAuth.middleware.js
@@ -2,22 +2,24 @@ import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
-export const requireSignin = async (req, res, next) => {
+export const checkAccessToken = async (req, res, next) => {
 	try {
-		const token = req.body.token || req.query.token || req.params.token || req.headers.token;
-		const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-		const { role } = await User.findOne({ _id: id }, { role: 1 });
-
+		const accessToken = req.body.token || req.params.token || req.headers.token || req.query.token;
+		const { id } = jwt.verify(accessToken, process.env.SECRET_KEY);
+		const { role } = await User.findOne({ _id: id }).select("role");
 		req.role = role;
 		req.auth = id;
 		next();
 	} catch (error) {
-		return res.status(401).json({
-			statusCode: 401,
-			message: "You haven't login yet!",
-		});
+		if (error.name === "TokenExpiredError")
+			return res.status(200).json({
+				error,
+				statusCode: 401,
+				message: "Require sign in !",
+			});
 	}
 };
+
 export const isAdmin = async (req, res, next) => {
 	if (req.role != 1)
 		return res.status(401).json({
