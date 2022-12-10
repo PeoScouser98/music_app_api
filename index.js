0797685ea5ebca8909a9ebd0f86291691c2ea3eb
Quang Hiệp <quanghiep03198@gmail.com>
2022-12-10 12:39:36 +0700

    export database json file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 
 /* ::::::::::::::::::::::: Connect to mongoDB ::::::::::::::::::::::: */
 mongoose
-	.connect(process.env.DATABASE_URI, { serverSelectionTimeoutMS: 5000 })
+	.connect(process.env.DATABASE_URI)
 	.then((res) => console.log("Connected to database!"))
 	.catch((err) => console.log(err));
 
@@ -17,7 +17,7 @@ mongoose
 // });
 
 /* ::::::::::::::::::::::: Run Server ::::::::::::::::::::::: */
-
-app.listen(process.env.PORT, () => {
-	console.log(`Connected! Server is listening on Port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Connected! Server is listening on: http://localhost:${PORT}/`);
 });
