497a8cff52ac1cd6e2696d61ba0c48c8f11aa71f
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-02 20:28:18 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import "dotenv/config";
 import app from "./app";
 import connectMongoDB from "./config/mongoDB.config";
-import http from "http";
 
 const PORT = process.env.PORT || 3001;
-const server = http.createServer(app);
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.info(`[SUCCESS] Server is listening on port ${PORT}`);
 });
 
