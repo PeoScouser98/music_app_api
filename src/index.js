86b6364d407d8ceba0d4493b12da660e0ff51194
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-02 20:37:22 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import app from "./app";
 import connectMongoDB from "./config/mongoDB.config";
-
+import http from "http";
 const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.info(`[SUCCESS] Server is listening on port ${PORT}`);
 });
 
