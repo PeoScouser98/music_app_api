a41b2f6d2df39a98812443a7ccd2dbf1d5745453
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-10 23:54:43 +0700

    update

--- src/api/controllers/playlist.controller.js
+++ src/api/controllers/playlist.controller.js
@@ -2,112 +2,112 @@ import createHttpError from "http-errors";
 import Playlist from "../models/playlist.model";
 
 export const list = async (req, res) => {
-	try {
-		const limit = req.query.limit || 10;
-		const skip = req.query.skip || 0;
-		const playlists = await Playlist.find({ public: true }).skip(skip).limit(limit).sort({ createdAt: -1 }).exec();
-		return res.status(200).json(playlists);
-	} catch (error) {
-		return res.json({
-			error: 404,
-			message: error.message,
-		});
-	}
+    try {
+        const limit = req.query.limit || 10;
+        const skip = req.query.skip || 0;
+        const playlists = await Playlist.find({ public: true }).skip(skip).limit(limit).sort({ createdAt: -1 }).exec();
+        return res.status(200).json(playlists);
+    } catch (error) {
+        return res.json({
+            error: 404,
+            message: error.message,
+        });
+    }
 };
 export const getPlaylistsByUser = async (req, res) => {
-	try {
-		const playlistsByUser = await Playlist.find({ creator: req.params.userId })
-			.populate({ path: "tracks", select: "thumbnail" })
-			.skip(req.query.skip || 0)
-			.limit(req.query.limit)
-			.exec();
-		return res.status(200).json(playlistsByUser);
-	} catch (error) {
-		console.log(error);
-		res.status(404).json({
-			message: "Cannot find user playlists!",
-		});
-	}
+    try {
+        const playlistsByUser = await Playlist.find({ creator: req.params.userId })
+            .populate({ path: "tracks", select: "thumbnail" })
+            .skip(req.query.skip || 0)
+            .limit(req.query.limit || 20)
+            .exec();
+        return res.status(200).json(playlistsByUser);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            message: "Cannot find user playlists!",
+        });
+    }
 };
 
 export const listPrivatePlaylistsByUser = async (req, res) => {
-	try {
-		const playlists = await Playlist.find({ creator: req.params.userId, public: false }).select();
-		return res.status(200).json(playlists);
-	} catch (error) {
-		return res.status(404).json({
-			status: 404,
-			message: "Cannot find user playlists!",
-		});
-	}
+    try {
+        const playlists = await Playlist.find({ creator: req.params.userId, public: false }).select();
+        return res.status(200).json(playlists);
+    } catch (error) {
+        return res.status(404).json({
+            status: 404,
+            message: "Cannot find user playlists!",
+        });
+    }
 };
 
 export const read = async (req, res) => {
-	try {
-		const playlist = await Playlist.findOne({ _id: req.params.id }).select("-__v -updatedAt -createdAt").exec();
-		return res.status(200).json(playlist);
-	} catch (error) {
-		console.log(error);
-		res.status(404).json({
-			message: "Playlist does not exist!",
-		});
-	}
+    try {
+        const playlist = await Playlist.findOne({ _id: req.params.id }).select("-__v -updatedAt -createdAt").exec();
+        return res.status(200).json(playlist);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            message: "Playlist does not exist!",
+        });
+    }
 };
 
 export const create = async (req, res) => {
-	try {
-		const newPlaylist = await new Playlist({ creator: req.auth, ...req.body }).save();
-		console.log(req.body);
-		return res.status(201).json(newPlaylist);
-	} catch (error) {
-		res.status(500).json({
-			message: "Failed to create new playlist!",
-		});
-	}
+    try {
+        const newPlaylist = await new Playlist({ creator: req.auth, ...req.body }).save();
+        console.log(req.body);
+        return res.status(201).json(newPlaylist);
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to create new playlist!",
+        });
+    }
 };
 
 export const updateTracksList = async (req, res) => {
-	try {
-		if (!req.body) throw createHttpError.BadRequest("Invalid track data!");
+    try {
+        if (!req.body) throw createHttpError.BadRequest("Invalid track data!");
 
-		const playlistHasThisTrack = await Playlist.findOne({ _id: req.params.id, tracks: req.body.track }).exec();
+        const playlistHasThisTrack = await Playlist.findOne({ _id: req.params.id, tracks: req.body.track }).exec();
 
-		if (!playlistHasThisTrack) {
-			const afterAdded = await Playlist.findOneAndUpdate(
-				{ _id: req.params.id },
-				{ $addToSet: { tracks: req.body.track } },
-				{ new: true },
-			);
-			return res.status(201).json(afterAdded);
-		}
+        if (!playlistHasThisTrack) {
+            const afterAdded = await Playlist.findOneAndUpdate(
+                { _id: req.params.id },
+                { $addToSet: { tracks: req.body.track } },
+                { new: true },
+            );
+            return res.status(201).json(afterAdded);
+        }
 
-		const afterRemoved = await Playlist.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $pull: { tracks: req.body.track } },
-			{ new: true, upsert: true },
-		);
-		return res.status(201).json(afterRemoved);
-	} catch (error) {
-		console.log(error.message);
-		res.status(200).json({
-			message: error.message,
-			status: error.status,
-		});
-	}
+        const afterRemoved = await Playlist.findOneAndUpdate(
+            { _id: req.params.id },
+            { $pull: { tracks: req.body.track } },
+            { new: true, upsert: true },
+        );
+        return res.status(201).json(afterRemoved);
+    } catch (error) {
+        console.log(error.message);
+        res.status(200).json({
+            message: error.message,
+            status: error.status,
+        });
+    }
 };
 
 export const deletePlaylist = async (req, res) => {
-	try {
-		if (!req.auth) throw createHttpError("You cannot delete this playlist!");
-		console.log(req.auth);
-		const deletedPlaylist = await Playlist.findOneAndDelete({ creator: req.auth, _id: req.params.id }).exec();
-		if (!deletedPlaylist) throw createHttpError("You cannot delete this playlist!");
+    try {
+        if (!req.auth) throw createHttpError("You cannot delete this playlist!");
+        console.log(req.auth);
+        const deletedPlaylist = await Playlist.findOneAndDelete({ creator: req.auth, _id: req.params.id }).exec();
+        if (!deletedPlaylist) throw createHttpError("You cannot delete this playlist!");
 
-		return res.status(204).json(deletedPlaylist);
-	} catch (error) {
-		res.status(200).json({
-			status: error.status,
-			message: error.message,
-		});
-	}
+        return res.status(204).json(deletedPlaylist);
+    } catch (error) {
+        res.status(200).json({
+            status: error.status,
+            message: error.message,
+        });
+    }
 };
--- src/api/models/playlist.model.js
+++ src/api/models/playlist.model.js
@@ -3,43 +3,44 @@ import mongooseAutoPopulate from "mongoose-autopopulate";
 import mongooseSlugGenerator, { slugify } from "mongoose-slug-generator";
 
 const playlistSchema = mongoose.Schema(
-	{
-		title: {
-			type: String,
-			require: true,
-			min: 4,
-		},
-		creator: {
-			type: mongoose.Schema.Types.ObjectId,
-			require: true,
-			ref: "Users",
-			autopopulate: { select: "_id username" },
-		},
-		slug: { type: String, slug: "title", unique: true },
-		tracks: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Tracks",
-				autopopulate: { select: "-__v" },
-			},
-		],
-		thumbnail: {
-			type: String,
-			default: "",
-		},
-		createAt: {
-			type: Date,
-			default: new Date().toLocaleDateString(),
-		},
-		public: {
-			type: Boolean,
-			default: true,
-		},
-	},
-	{
-		timestamps: true,
-		strictPopulate: false,
-	},
+    {
+        title: {
+            type: String,
+            require: true,
+            min: 4,
+        },
+        creator: {
+            type: mongoose.Schema.Types.ObjectId,
+            require: true,
+            ref: "Users",
+            autopopulate: { select: "_id username" },
+        },
+        slug: { type: String, slug: "title", unique: true },
+        tracks: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Tracks",
+                autopopulate: { select: "-__v" },
+            },
+        ],
+        thumbnail: {
+            type: String,
+            default:
+                "https://firebasestorage.googleapis.com/v0/b/music-app-cdef5.appspot.com/o/pictures%2Fdefault-album-image.png?alt=media&token=3c078580-13d5-4252-9c35-ab1d30deefeb",
+        },
+        createAt: {
+            type: Date,
+            default: new Date().toLocaleDateString(),
+        },
+        public: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+        strictPopulate: false,
+    },
 );
 
 playlistSchema.plugin(mongooseAutoPopulate, mongooseSlugGenerator);
