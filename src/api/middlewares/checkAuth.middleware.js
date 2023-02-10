efdb5171f1b0476addec0b7eaef861fd60e02759
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-11 00:08:33 +0700

    fix checkauth middleware

--- src/api/middlewares/checkAuth.middleware.js
+++ src/api/middlewares/checkAuth.middleware.js
@@ -6,30 +6,30 @@ import path from "path";
 import User from "../models/user.model";
 
 export const checkAccessToken = async (req, res, next) => {
-	try {
-		const accessToken = req.headers.authorization.split(" ").at(1);
-		if (!accessToken) throw createHttpError.Unauthorized("Access token must be provided!");
+    try {
+        if (!accessToken) throw createHttpError.Unauthorized("Access token must be provided!");
+        const accessToken = req.headers.authorization.split(" ").at(1);
 
-		const certification = await readFileSync(path.resolve("public.crt"));
-		const { credential } = jwt.verify(accessToken, certification, { algorithms: "RS256" });
-		const { role } = await User.findOne({ _id: credential }).select("role");
-		req.role = role;
-		req.auth = credential;
+        const certification = await readFileSync(path.resolve("public.crt"));
+        const { credential } = jwt.verify(accessToken, certification, { algorithms: "RS256" });
+        const { role } = await User.findOne({ _id: credential }).select("role");
+        req.role = role;
+        req.auth = credential;
 
-		next();
-	} catch (error) {
-		console.log("[ERROR] at checkAuth middleware:>>>", error.message);
-		return res.status(401).json({
-			status: 401,
-			message: error.message,
-		});
-	}
+        next();
+    } catch (error) {
+        console.log("[ERROR] at checkAuth middleware:>>>", error.message);
+        return res.status(401).json({
+            status: 401,
+            message: error.message,
+        });
+    }
 };
 
 export const isAdmin = async (req, res, next) => {
-	if (req.role != 1)
-		return res.status(401).json({
-			message: "Unauthorized error! You are not admin!",
-		});
-	next();
+    if (req.role != 1)
+        return res.status(401).json({
+            message: "Unauthorized error! You are not admin!",
+        });
+    next();
 };
