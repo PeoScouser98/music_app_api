53e32cc5d8a6fb034581ad5220936281aa511396
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-23 23:45:18 +0700

    install webpack & vercel config & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,55 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
-    entry: "./src/index.js",
-    target: "node",
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            },
-        ],
-    },
-    externals: [
-        {
-            nock: "commonjs2 nock",
-            "mock-aws-s3": "commonjs2 mock-aws-s3",
-            "node-pre-gyp": "node-pre-gyp",
-        },
-    ],
-    devtool: false,
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	mode: "production",
+	target: "node",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+				},
+				extractComments: false,
+			}),
+		],
+	},
+	externals: {
+		bcrypt: "commonjs bcrypt",
+		compression: "commonjs compression",
+		cors: "commonjs cors",
+		dotenv: "commonjs dotenv",
+		express: "commonjs express",
+		"http-errors": "commonjs http-errors",
+		jsonwebtoken: "commonjs jsonwebtoken",
+		mongoose: "commonjs mongoose",
+		"mongoose-autopopulate": "commonjs mongoose-autopopulate",
+		"firebase-admin": "commonjs firebase-admin",
+		"mongoose-slug-generator": "commonjs mongoose-slug-generator",
+		morgan: "commonjs morgan",
+		googleapis: "commonjs googleapis",
+		nodemailer: "commonjs nodemailer",
+	},
 };
